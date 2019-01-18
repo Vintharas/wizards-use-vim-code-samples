1ebf83d7dbb5c89def19bc6bd4a37ea6e71518b5
Jaime González García <jaime.gg83@gmail.com>
2019-01-18 14:28:02 +0100

    Added new sample for chapter on motions

---
+++ 003-fireball-spell.js
@@ -0,0 +1,11 @@
+export const fireballSpell = {
+  manaCost: 10,
+  name: "Fireball",
+  type: "Fire",
+  description:
+    "A bright streak flashes from your pointing finger to a point you choose within range and then blossoms with a low roar into an explosion of flame.",
+  damage: "d20",
+  modifier: 8,
+  range: 20,
+  area: 5
+};
